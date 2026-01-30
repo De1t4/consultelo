741b95e8350922d7b2367ec896197e75fbc0030d
De1t4 <mauriciochambicaceres@gmail.com>
2026-01-30 13:38:01 -0300

    feat: Set up initial Supabase integration including client, server, and proxy for Next.js.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import { createClient } from "../../core/lib/server";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: consultations } = await supabase.from("consultations").select();
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        {
+          consultations?.map((consultation) => (
+            <div key={consultation.id}>
+              <h1>{consultation.title}</h1>
+              <p>{consultation.body}</p>
+            </div>
+          ))
+        }
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -14,7 +26,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Consultelo
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}

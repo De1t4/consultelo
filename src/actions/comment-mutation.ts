37d7aec7d5e789648378d8448d22ef70eaba7beb
De1t4 <mauriciochambicaceres@gmail.com>
2026-03-04 13:22:20 -0300

    fix:error server actions prod

--- src/actions/comment-mutation.ts
+++ src/actions/comment-mutation.ts
@@ -5,25 +5,27 @@ import { createComment, isCommentedByUser } from "@/services/comment-service";
 import { authOptions } from "@/shared/lib/auth";
 import { getServerSession } from "next-auth";
 import { cookies } from "next/headers";
-import { createServerAction } from "./test";
+import { ServerActionError, createServerAction } from "./test";
 import { executeAction } from "@/shared/types/executionAction";
 
 export const createCommentAction = createServerAction(
   async (data: FormDataComment) => {
     if (data.consultationId === undefined) {
-      throw new Error("No consultation ID was provided.");
+      throw new ServerActionError("No consultation ID was provided.");
     }
     const cookieStore = await cookies();
     const cookieName = `has_commented_${data.consultationId}`;
     const session = await getServerSession(authOptions);
 
     if (cookieStore.get(cookieName)) {
-      throw new Error("You have already left a comment on this consultation.");
+      throw new ServerActionError(
+        "You have already left a comment on this consultation.",
+      );
     }
 
     if (!data.isAnonymous) {
       if (!session?.user?.id) {
-        throw new Error("You must be logged in to comment.");
+        throw new ServerActionError("You must be logged in to comment.");
       }
       data.userId = session.user.id;
       const commented = await isCommentedByUser(
@@ -31,7 +33,7 @@ export const createCommentAction = createServerAction(
         data.consultationId,
       );
       if (commented) {
-        throw new Error(
+        throw new ServerActionError(
           "You have already left a comment on this consultation.",
         );
       }

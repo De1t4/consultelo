95e85c1f9b2c3bceba43d8dbaedb4c9be6462509
De1t4 <mauriciochambicaceres@gmail.com>
2026-03-16 18:14:32 -0300

    bug: fix bug other recipients response email

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
     }
 
     const { data, error } = await resend.emails.send({
-      from: "Consultelo <onboarding@resend.dev>",
+      from: "Consultelo <[EMAIL_ADDRESS]>",
       to: process.env.EMAIL_ADDRESS || "delivered@resend.dev",
       subject: `New Contact | Subject: ${subject} | From: ${email}`,
       html: `
